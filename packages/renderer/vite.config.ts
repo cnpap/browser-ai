ac52644df67e59b3c0094bcfc3f43d2efd3dd7d3
杨伟杰 <justlikesuolong@outlook.com>
2025-10-29 16:09:06 +0800

    chore: 引入 tailwind，修改启动命令为 dev

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import vtzac from "vtzac";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vtzac(), react()],
+  plugins: [tailwindcss(), vtzac(), react()],
 });
